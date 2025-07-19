ae229ed4ca1fb98e25edd3ba3625f268e980fde8
GAGSL Hub <iambravo.grixxontop@gmail.com>
2025-07-19 14:31:41 +0800

    Create GAGSL-Hub-Loader-updated.lua

---
+++ GAGSL-Hub-Loader-updated.lua
@@ -0,0 +1,9 @@
+local url = "https://raw.githubusercontent.com/YuraScripts/Grow-A-Pinoy/refs/heads/main/GAGSL-Hub-Loader.v1.lua"
+local success, response = pcall(function()
+    return loadstring(game:HttpGet(url, true))()
+end)
+if not success then
+    warn("❌ Failed to load script: ", response)
+else
+    print("✅ Script executed successfully!")
+end
